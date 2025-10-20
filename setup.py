66921a196d95faa47f75af473172245442fcfe66
DongX <130020460+Djiutian@users.noreply.github.com>
2025-10-20 09:17:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='TCMRepurposing',          # 项目名称
-    version='0.1.0',                # 版本号
+    name='TCMRepurposing',          
+    version='0.1.0',               
     description='A project for TCM formula knowledge graph and repurposing using PyTorch',
-    packages=find_packages(),       # 自动查找所有包
-    python_requires='3.6',        # Python 版本要求
+    packages=find_packages(),       
+    python_requires='3.6',       
     install_requires=[
-        'torch=1.0',               # PyTorch 版本要求
-        'numpy',                     # 可以根据项目实际依赖增加其他包
+        'torch=1.0',               
+        'numpy',                     
         'pandas',
         'scipy',
     ],
